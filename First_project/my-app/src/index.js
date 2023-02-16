cdf86c0405c59a9ec20fb62dd967209d3147187d
QHT <quoc.huan.tran@gmail.com>
2023-02-16 13:04:43 -0500

    Learning basic

--- First_project/my-app/src/index.js
+++ First_project/my-app/src/index.js
@@ -1,58 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import './index.css';
+import "./index.css"
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
-
-function Header(){
-  return(
-    <header>
-        <nav>
-          <img src="./logo192.png" width="40px"></img>
-          <ul className="nav-items">
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-    </header>
-  )
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
 }
-
-function Footer(){
-  return(
-    <footer>© 20xx Test development. All rights reserved.</footer>
-  )
-}
-
-function List(){
-  return(
-    <div>
-      <h1>Function List</h1>
-      <ol>
-        <li>This is a funct</li>  
-        <li>This is a funct</li>
-        <li>This is a funct</li>
-        <li>This is a funct</li>  
-        <li>This is a funct</li>
-      </ol>
-    </div>
-  )
-}
-
-function Page(){
-  return (
-    <div>
-      <Header />
-      <List />
-      <Footer />
-    </div>
-  )
-}
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Page />)
-
-
-
-
+root.render(<App />)
