3e57543f02c2e2848b6d5764632394fc07c59a93
QHT <quoc.huan.tran@gmail.com>
2023-03-09 15:27:33 -0500

    End of airbnb exercice

--- 02_Data_Driven_React/DataDriven/src/component/MainSection.jsx
+++ 02_Data_Driven_React/DataDriven/src/component/MainSection.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Card from "./Card";
 
 export default function MainSection() {
     return (
         <div className="main-container">
-
+            <Card />
         </div>
     );
 }
