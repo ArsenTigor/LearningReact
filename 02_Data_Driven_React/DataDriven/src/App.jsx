07e98bf4344c480b3ec6014ae4e8a825929e6a01
ArsenTigor <quoc.huan.tran26@gmail.com>
2023-03-13 16:14:21 -0400

    Finish airbnb practice

--- 02_Data_Driven_React/DataDriven/src/App.jsx
+++ 02_Data_Driven_React/DataDriven/src/App.jsx
@@ -1,16 +1,31 @@
 import react from 'react' 
 import Navbar from './component/Navbar'
 import Hero from './component/Hero'
-import MainSection from './component/MainSection'
+import Card from './component/Card'
+import Data from './Data'
 
 export default function App() {
+  const cards = Data.map(item => {
+    return(
+      <Card
+        key={item.id}
+        item={item}
+      />
+    )
+  })
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <MainSection />
+      <div className='main-container'>
+        {cards}
+      </div>
+      
+
+      
     </div>
   )
 }
+
 
 
--- 02_Data_Driven_React/DataDriven/src/component/Card.jsx
+++ 02_Data_Driven_React/DataDriven/src/component/Card.jsx
@@ -1,37 +1,25 @@
 import React from "react"
 import Star from "/assets/star.png"
 
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
 export default function Card(props) {
+    let item = props.item
+    let badgeText 
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.item.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
-            <img src={`../assets/${props.img}`} alt="Katie Zaferes" />
-            
-            
+            {badgeText && <div className="card-badge">{item.badgeText}</div>}
+            <img src={`../assets/${item.coverImg}`} />
             <div className="rating"> 
                 <img src={Star} alt="Star" />   
-                <p> {props.rating} ({props.reviewCount}) * {props.country}</p>
+                <p> {item.stats.rating} ({item.stats.reviewCount}) {item.location}</p>
             </div>
-            <p> {props.title}</p>
-            <p> <b>From {props.price}</b> /per person </p>
+            <p> {item.title}</p>
+            <p> <b>From {item.price}</b>$ /per person </p>
         </div>
     )
 }
