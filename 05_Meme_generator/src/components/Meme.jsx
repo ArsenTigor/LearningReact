dee590e8713bb5db51cfcb7ed97ddcd674dba1e5
ArsenTigor <quoc.huan.tran26@gmail.com>
2023-03-14 11:34:51 -0400

    Starting meme_generator project, dynamic website tutorial part

---
+++ 05_Meme_generator/src/components/Meme.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input 
+                    type="text"
+                    className="form--inputs"
+                    placeholder="Top text"
+                />
+                <input 
+                    type="text"
+                    className="form--inputs"
+                    placeholder="Bottom text"
+                />
+                <button className="form--button">Generate a meme image</button>
+
+    
+            </form>
+        </main>    
+    )
+}
