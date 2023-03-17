a80933657726646d6bb288b4af1748c4873bd749
ArsenTigor <quoc.huan.tran26@gmail.com>
2023-03-17 11:54:12 -0400

    Adding text to the meme

--- 05_Meme_generator/src/components/Meme.jsx
+++ 05_Meme_generator/src/components/Meme.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import memesData from "../memesData";
 
-
-
-
-
 export default function Meme() {
 
     let allMeme = memesData["data"]["memes"];
@@ -24,10 +20,12 @@ export default function Meme() {
         bottomText: "",
         url: "http://i.imgflip.com/1bij.jpg"
     })
-    
 
+    function handleChange(event){
+        const {name, value} = event.target
+        setCurrentMeme(prevData => ({...prevData, [name]: value}))
+    }
     
-
     return (
         <main>
             <div className="form">
@@ -35,18 +33,28 @@ export default function Meme() {
                     type="text"
                     className="form--inputs"
                     placeholder="Top text"
+                    name="topText"
+                    onChange={handleChange}
+                    value={currentMeme.topText}
                 />
                 <input 
                     type="text"
                     className="form--inputs"
                     placeholder="Bottom text"
+                    name="bottomText"
+                    onChange={handleChange}
+                    value={currentMeme.bottomText}
                 />
                 <button className="form--button" 
                     onClick={getRandomMeme}>
                     Generate a meme image
                 </button>
             </div>
-            <img className="meme--image" src={currentMeme["url"]}></img>
+            <div className="meme">
+                <img className="meme--image" src={currentMeme["url"]}></img>
+                <h2 className="meme--text top">{currentMeme.topText}</h2>
+                <h2 className="meme--text bottom">{currentMeme.bottomText}</h2>
+            </div>
 
         </main>    
     )
